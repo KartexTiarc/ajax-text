4aee53d27663938d437564eca4ba4aa1dadcded7
KartexTiarc <christoffer.nyh@hotmail.com>
2021-01-28 22:21:02 +0000

    need to continue with project

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -24,12 +24,26 @@ function getTableHeaders(obj) {
    return `<tr>${tableHeaders}</tr>`;
 }
 
+function generatePaginationButtons (next, prev) {
+    if (next && prev) {
+        return `<button onclick ="writeToDocument('${prev}')">Previous</button>`;
+                `<button onclick ="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev){
+        return `<button onclick ="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick ="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
 function writeToDocument(type) {
     var el = document.getElementById("data");
     el.innerHTML = "";
     
     getData(type, function (data){
-        var tableRows = [];
+        var pagination;
+        if (data.next || data.previous){
+            pagination = generatePaginationButtons(data.next, data.previous)
+        }
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
@@ -44,6 +58,6 @@ function writeToDocument(type) {
           tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }
